64d384ab3f28c49448f3146856a2e0fda70b29b7
hriordan <mightofh@gmail.com>
2014-04-20 15:15:41 -0500

    Barest of shell of OM node class

---
+++ nodes.py
@@ -0,0 +1,22 @@
+#from enum import Enum
+
+class orders:
+    attack = 1
+    retreat = 2
+    tie = 3
+    
+
+
+class node:
+    'general node-processor class' 
+    nodeCount = 0
+    
+    def __init__(self,id, loyal):
+        self.id = name
+        self.loyal = loyal #bool
+        node.nodeCount += 1
+        self.order = orders.retreat #default to retreat?
+
+
+
+        
