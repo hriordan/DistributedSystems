c72049005001b3053e9d64b31760cf69afa8d514
hriordan <mightofh@gmail.com>
2014-04-28 00:06:51 -0500

    Need to find way to safely, nondestructively trim generals lists

--- nodes.py
+++ nodes.py
@@ -6,7 +6,7 @@ class orders:
     
 
 
-class Node:
+class General:
     'general node-processor class' 
     nodeCount = 0
     
@@ -18,7 +18,7 @@ class Node:
         else:
             self.loyal = False
 
-        node.nodeCount += 1
+        General.nodeCount += 1
         
         self.order = orders.retreat #default to retreat?
         
@@ -33,7 +33,8 @@ class Node:
 
 
     def tell_orders(self, generals, prefix):
-        key = ''.join([prefix,self.id])
+
+        key = ''.join([prefix, str(self.id)])
         self.ordict[key] = self.ordict[prefix] #"tell yourself what you are talking about"
 
         if self.loyal == True: 
@@ -72,12 +73,12 @@ def run_generals(orderset):
 
 
 def generals(order):
-    m = order[0]
+    m = int(order[0])
     
     generals = [] 
     for i in range(len(order[1])): 
         generals.append( General(i, order[1][i]) )   #create general list 
-        orders.nodeCount += 1
+        General.nodeCount += 1
 
     if order[2] == "ATTACK":
         generals[0].ordict['_'] = orders.attack  #initialize/seed alpha-commander's knowledge of own command 
@@ -87,7 +88,7 @@ def generals(order):
         print "Not a valid initial order for the commander" 
         exit(0)
 
-    _generals_h('_', m, generals[0], generals[2:])
+    _generals_h('_', m, generals[0], generals[1:])
 
     #check results
     for gen in generals:
@@ -98,9 +99,7 @@ def generals(order):
 
 def _generals_h(prefix, m, commander, generals): #whatdo
     
-    newfix = ''.join([prefix,commander.id])
-
-
+    newfix = ''.join([prefix,str(commander.id)])
 
     if m == 0:
         commander.tell_orders(generals,prefix)
@@ -108,16 +107,18 @@ def _generals_h(prefix, m, commander, generals): #whatdo
 
     else: 
         commander.tell_orders(generals, prefix)
-
+        
         #exchange order knowledge
         for gen in generals:
+            #LOOKS LIKE I MAY HAVE TO MANUALLY CONSTRUCT SUBLISTS...
             _generals_h(newfix,m-1,gen,generals.remove(gen)) #will the generals argument be a local copy? please say yes
-
+            
         #do majority:
         for gen in generals:
             gen.ordict[prefix] = majority(gen, generals.remove(gen), prefix) #again, ensure the scope here. 
             gen.orders = gen.ordict[prefix]
 
+       
         #return generals
     return 
 
